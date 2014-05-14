cb6004b83fcd9d00f8aa0f30bb56c1dd0a5e0cb8
Pedro Santos <donbonifacio@gmail.com>
2014-05-14 09:30:06 +0100

    Bootstrap mac with brew

---
+++ brewboot.sh
@@ -0,0 +1,18 @@
+# openssl 
+# LDFLAGS:  -L/usr/local/opt/openssl/lib
+# CPPFLAGS: -I/usr/local/opt/openssl/include
+
+brew install tmux
+brew install rbenv
+brew install ruby-build
+brew install mysql
+brew install wget
+
+brew tap homebrew/dupes
+brew install apple-gcc42
+
+brew install caskroom/cask/brew-cask
+brew cask install google-chrome
+brew cask install adium
+brew cask install hipchat
+brew cask install sequel-pro
