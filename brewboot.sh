1674a1978780de04c1617ef7408aa392902737c8
Pedro Santos <donbonifacio@gmail.com>
2014-09-17 21:44:41 +0100

    Add fswatch

--- brewboot.sh
+++ brewboot.sh
@@ -14,6 +14,7 @@ brew install hub
 brew install weechat --with-lua --with-perl --with-python --with-ruby
 brew install git-extras
 brew install phantomjs
+brew install fswatch
 
 brew tap homebrew/dupes
 brew install apple-gcc42
