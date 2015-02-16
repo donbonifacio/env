ef83930c1a708b3d1cec90c3c13ae9cfae711417
Pedro Santos <donbonifacio@gmail.com>
2015-02-16 17:12:58 +0000

    Update syscope

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,6 +1,6 @@
 {:user {:plugins [[venantius/ultra "0.1.9"]]
-         :ultra {:color-scheme :solarized_dark}
-        :dependencies [[spyscope "0.1.3"]
+        :ultra {:color-scheme :solarized_dark}
+        :dependencies [[spyscope "0.1.5"]
                        [redl "0.2.4"]]
         :injections [(require 'spyscope.core)
                      (require '[redl complete core])]}}
