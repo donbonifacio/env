c3e8b6bcb73be5f7601df2e8d9a79541074e65e7
Pedro Santos <donbonifacio@gmail.com>
2015-02-16 17:21:31 +0000

    Add clojure tools trace

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,6 +1,8 @@
 {:user {:plugins [[venantius/ultra "0.1.9"]]
         :ultra {:color-scheme :solarized_dark}
         :dependencies [[spyscope "0.1.5"]
+                       [org.clojure/tools.trace "0.7.5"]
                        [redl "0.2.4"]]
         :injections [(require 'spyscope.core)
+                     (require 'clojure.tools.trace)
                      (require '[redl complete core])]}}
