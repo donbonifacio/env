8734903eb2665abc12d82edd21118da594faddfe
Pedro Santos <donbonifacio@gmail.com>
2015-03-16 22:39:29 +0000

    Add debugger

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,7 +2,9 @@
         :ultra {:color-scheme :solarized_dark}
         :dependencies [[spyscope "0.1.5"]
                        [org.clojure/tools.trace "0.7.5"]
+                       [debugger "0.1.4"]
                        [redl "0.2.4"]]
         :injections [(require 'spyscope.core)
+                     (use 'debugger.core)
                      (require 'clojure.tools.trace)
                      (require '[redl complete core])]}}
